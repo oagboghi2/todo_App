4096a5334bbf2448653b817c2d89d006a422b22d
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2018-02-12 16:15:13 -0800

    Fixed delete route, and added completedness check

--- public/app.js
+++ public/app.js
@@ -12,19 +12,16 @@ $(document).ready(function(){
       }
     })
 
-    $(".list").on('click', 'span', function(){
-      var ClickId = $(this).parent().data('id');
-      var deleteUrl = '/api/todo/' + ClickId;
-      $.ajax({
-        method: 'DELETE',
-        url: deleteUrl
-      })
-        .then(function(data){
-          console.log(data);
-        })
-        $(this).parent().remove();
+    $(".list").on('click','li', function(){
+      console.log($(this));
+      updateTodo($(this))
+    })
 
+    $(".list").on('click', 'span', function(event){
+      event.stopPropagation();
+      removeTodo($(this).parent());
     });
+
 });
 
 function addTodos(todos){
@@ -37,8 +34,8 @@ function addTodos(todos){
 
 function addTodo(todo){
   var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
-  newTodo.data('id', todo._id)
-  
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   $('.list').append(newTodo);
 }
 
@@ -52,5 +49,37 @@ function createTodo(){
     })
     .catch(function(err){
       console.log(err);
+    });
+}
+
+
+function removeTodo(todo){
+  var ClickId = todo.data('id');
+  var deleteUrl = '/api/todo/' + ClickId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+    .then(function(data){
+      todo.remove();
     })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todo/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    console.log(updatedTodo);
+    todo.toggleClass("done")
+    todo.data('completed', isDone)
+  })
 }
